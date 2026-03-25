d60717180b9fb72a0f77a4a05dba82664fd85160
antifragilestong <antifragilestrong@gmail.com>
2026-03-26 02:07:25 +0800

    push Javascript functions assignment

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,15 @@
+
+// function assignment
+
+
+function My_First_Function() {
+    var ABC= 'This is the botton text *#$  %@';
+    document.getElementById("Button_Text").innerHTML=ABC;
+}
+
+// a function that uses the += operator
+function myFun_tion() {
+    var sentence="I am learning"; // creat a variable
+    sentence += " Javascript from Pitman.";  // use += to add another variable
+    document.getElementById("Concatenate").innerHTML = sentence;//using document.getElementById()
+}
